7c2eaa2bd787512d03501ce927a9c8e9ca51a157
Matt Porter <mporter@weebly.com>
2015-09-25 15:10:21 -0700

    updated files to use spaces rather than hard tabs. formatted code to weebly syntax styling standards.

--- files/js/element.js
+++ files/js/element.js
@@ -2,49 +2,48 @@
  * This is required for element rendering to be possible
  * @type {PlatformElement}
  */
-(function(){
-	var TeamCard = PlatformElement.extend({
-		initialize: function() {
-			$(document).ready(function() {
-				this.fixStyles();
-			}.bind(this));
+(function() {
+    var TeamCard = PlatformElement.extend({
+        initialize: function() {
+            $(document).ready(function() {
+                this.fixStyles();
+            }.bind(this));
 
-			this.fixStyles();
-		},
+            this.fixStyles();
+        },
 
-		/**
-		 * Lots of styles are applied by default to editable areas of
-		 * the editor. To make the element looks how you want, some styles
-		 * need to be overwritten.
-		 *
-		 * Classes that are used are:
-		 * 		- .editable-text
-		 * 		- .paragraph
-		 * 		- .ui-wrapper
-		 * 		- .wsite-image
-		 * 		- .wsite-*
-		 * 		- (etc...)
-		 */
-		fixStyles: function() {
-			var w = this.$el.find('.team-card__image--round').width();
-			var h = w;
+        /**
+         * Lots of styles are applied by default to editable areas of
+         * the editor. To make the element looks how you want, some styles
+         * need to be overwritten.
+         *
+         * Classes that are used are:
+         *      - .editable-text
+         *      - .paragraph
+         *      - .ui-wrapper
+         *      - .wsite-image
+         *      - .wsite-*
+         *      - (etc...)
+         */
+        fixStyles: function() {
+            var w = this.$el.find('.team-card__image--round').width();
+            var h = w;
 
-			this.$el.find('.editable-text').each(function(index) {
-				$(this).attr('style', '');
-			});
+            this.$el.find('.editable-text').each(function(index) {
+                $(this).attr('style', '');
+            });
 
-			this.$el.find('.element').each(function(index) {
-				$(this).attr('style', '');
-			});
+            this.$el.find('.element').each(function(index) {
+                $(this).attr('style', '');
+            });
 
-			this.$el.find('img').css({
-				display: '',
-				width: '',
-				height: ''
-			});
-		}
-	});
+            this.$el.find('img').css({
+                display: '',
+                width: '',
+                height: ''
+            });
+        }
+    });
 
-	return TeamCard;
+    return TeamCard;
 })();
-
